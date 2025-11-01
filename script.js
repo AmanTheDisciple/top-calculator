7cf2b9bb1f90b474bb328d8fd2af19f8f2a441bd
Aman <82828343+AmanTheDisciple@users.noreply.github.com>
2025-11-01 19:37:30 +0000

    add event listeners to buttons number,clear and clear-all and view of inputBox

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-let num1 = 0;
-let num2 = 0;
+let num1 = '';
+let num2 = '';
 let operator = '';
+let num1Sign=''; //Denoting whether the first number is negative or positive
+let lastInputFlag=''; //Denoting last input (num1,operator or num2). Helps when clearing the last digit or operator.
+let inputBox=document.querySelector('.input');
 
 function add(num1,num2){
     return +num1 + +num2;
@@ -32,8 +35,41 @@ function operate(num1,num2,operator){
     }
 }
 
-document.querySelector('.number').addEventListener('click',(e)=>{
-    if(num2==0 && operator==''){
-        num1 = (num1 * 10) + +e.target.textContent;
+document.querySelector('.buttons').addEventListener('click',(e)=>{
+    let classList=e.target.classList;
+
+    if(classList.contains('number')){
+
+        if(operator==''){
+            num1 += e.target.textContent;
+        }else{
+            num2 += e.target.textContent;
+        }
+        inputBox.textContent += e.target.textContent;
+        console.log('number listened');
+
+    }else if(classList.contains('clear-all')){
+        num1='';
+        num2='';
+        operator='';
+        inputBox.textContent='';
+        console.log('clear-all listened');
+    }else if(classList.contains('clear')){
+        switch(lastInputFlag){
+            case 'num1sign':
+
+            case 'num1':
+                num1 = num1.slice(0,-1);
+            case 'operator':
+                operator='';
+            case 'num2':
+                num2 = num2.slice(0,-1);    
+        }
+        inputBox.textContent=inputBox.textContent.slice(0,-1);   
+        console.log('clear listened');
     }
-})
+});
+
+// num1='-2.4';
+// console.log(+num1);
+
